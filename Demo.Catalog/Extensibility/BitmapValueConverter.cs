ae4e377ab6a48e389b5e6833691523dfe5e3c7dc
daniel <kidusdaniel@outlook.com>
2022-07-07 05:00:38 +0300

    image converter added

---
+++ Demo.Catalog/Extensibility/BitmapValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Catalog.Extensibility
+{
+    public class BitmapValueConverter : IValueConverter
+    {
+        public static BitmapValueConverter Instance = new BitmapValueConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value!=null)
+                value = new Bitmap(value.ToString());
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
